ac595f82debe4d8d08765e954f33b26fe3d6e219
Andrei Boghean <andreiboghean00+github@gmail.com>
2025-01-05 18:05:07 +0000

    functioning heartrate measurement and display on infinitime side in preparation for heartrate app

--- common_srcs/externApp.cpp
+++ common_srcs/externApp.cpp
@@ -1,27 +1,22 @@
 #include "compat.h"
 #include "stdint.h"
-#include <cstdio>
-
-void pollHeartRate()
-{
-  int bpm = get_hr_bpm();
-  char bpm_s[4];
-  // sprintf(bpm_s, "%03d", bpm);
-  if (bpm) bpm = 0;
-  bpm_s[0] = '1';
-  bpm_s[1] = '2';
-  bpm_s[2] = '3';
-  bpm_s[3] = '\0';
-  place_label(bpm_s, 100, 100);
-}
 
 int numba2 = 0;
-void temp2() {
-  clear_screen();
-  numba2 = (numba2+1)%7;
+void pollHeartRate()
+{
+  // numba2 = (numba2+1)%7;
+  // int bpm = 120+numba2; // get_hr_bpm();
+  
+  int bpm = get_hr_bpm();
 
-  show_int(numba2);
-  return;
+  char bpm_s[4];
+  bpm_s[0] = (char) 48+(bpm / 100);
+  bpm_s[1] = (char) 48+((bpm % 100) / 10);
+  bpm_s[2] = (char) 48+(bpm % 10);
+  bpm_s[3] = '\0';
+
+  clear_screen();
+  place_label(bpm_s, 100, 70);
 }
 
 
@@ -30,8 +25,8 @@ int extern_main()
   set_colours(0x00FF00, 0xFFFF00);
   // TODO: CHANGE THE DEFAULT FG COLOUR SO IT'S NOT BLACK
   
-  // start_read_hr();
-  // register_timer_interrupt(pollHeartRate, 1000);
-  register_timer_interrupt(temp2, 1000);
+  start_read_hr();
+  register_timer_interrupt(pollHeartRate, 1000);
+  // register_timer_interrupt(temp2, 1000);
   return 0;
 }
