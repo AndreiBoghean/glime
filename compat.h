dc6b81c5ee3aa1c4700b2bed30c025df261a6ef4
AndreiBoghean <67372068+AndreiBoghean@users.noreply.github.com>
2025-02-17 13:12:45 +0000

    add comments to compat.h

--- compat.h
+++ compat.h
@@ -2,11 +2,22 @@
 
 #include <stdint.h>
 
+/*
+  notes:
+  valid positions for x/y arguments are 0 <= x/y <= 240.
+  0,0 is top left.
+
+  the background and foreground colours are by default white on black,
+  and are used for the default where possible e.g. place_label, show_int, draw_rect.
+*/
+
+// init function with room for arbitrary args. this is for internal use by the implementer of glime within a watch OS, and should not concern most people.
 void init(void* setups);
-void show_int(int i);
 
 // places a label centrered at x, y.
 void place_label(const char* string, int x, int y);
+// places an integer in the centre of the screen. mostly for debugging. only supports integers 0-9.
+void show_int(int i);
 
 enum buttonTouchEvent {btn_shortClick, btn_released, btn_pressLost, btn_longPress, btn_longPressRepeat};
 typedef void (*buttonCallback) (enum buttonTouchEvent);
@@ -17,8 +28,9 @@ int counter_get(void* counter);
 void counter_set(void* counter, int newVal);
 void counter_render(void* counter);
 
-void set_colours(uint32_t fg, uint32_t bg);
 // set the foreground and background colours for drawing.
+// arguments are in the form of hexadecimal, e.g. set_colours(0x0000FF, 0xFFFF00) to set to blue foreground on yellow background. 
+void set_colours(uint32_t fg, uint32_t bg);
 
 enum brightness_level {lo, med, hi};
 void set_brightness(enum brightness_level bl);
@@ -28,8 +40,9 @@ typedef int (*touchCallback)(enum globalTouchEvent e);
 void register_global_eventListener(touchCallback e);
 // ^ callback returns a boolean according to whether it actioned on the event or not
 // if it DID action, then we finalise that "event" and move on to detecting the next one.
-// i.e. we use the return to decide whether to call lvgl.CancelTap();
+// internal note: we use the return to decide whether to call lvgl.CancelTap();
 
+// similar as above, but detects solely tap events, and makes available the x/y positions of the event. 
 typedef int (*touchCallback_xy)(uint16_t x, uint16_t y);
 void register_global_eventListener_xy(touchCallback_xy e);
 
@@ -43,14 +56,14 @@ void start_read_hr();
 void stop_read_hr();
 int get_hr_bpm();
 
+// a timer_interrupt should be used when the application is actively running; for passive background timers see below.
 typedef void (*timer_interrupt_callback)();
 void register_timer_interrupt(timer_interrupt_callback action, int period_ms);
 
+// these can be used for proper background timers that still countdown even when the application is backgrounded or the watch goes to sleep.
 void* timer_create(timer_interrupt_callback action, int period_ms);
 void timer_setPeriod(void* timer, int newPeriod);
 void timer_start(void*);
 void timer_stop(void*);
 int timer_isRunning(void* timer);
 int timer_msRemaining(void* timer);
-
-// void destruct();
